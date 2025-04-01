554864aa256eb9eba0df543309d5be7ba234eb50
milesfchen <fchen2@SB000812-2.local>
2025-04-01 18:10:17 -0500

    set up docker container

--- benchmark/my_eeg_conformer/preprocessing/seizure_data.py
+++ benchmark/my_eeg_conformer/preprocessing/seizure_data.py
@@ -9,12 +9,12 @@ from torch.utils.data import DataLoader, Dataset
 import torchvision.models as models
 
 
-class SeizureData():
+class SeizureData:
     def __init__(self, edf_file, label_file, window_size_sec=25, fs=256):
         super(SeizureData, self).__init__()
-        self.edf_file = edf_file    # carry the edf file name with the entity
-        self.label_file = label_file # carry the label file name with the entity
-        
+        self.edf_file = edf_file  # carry the edf file name with the entity
+        self.label_file = label_file  # carry the label file name with the entity
+
         self.eeg = Eeg.loadEdfAutoDetectMontage(edfFile=edf_file)
         if self.eeg.montage is not Eeg.Montage.UNIPOLAR:
             raise Exception("Error: Only unipolar montages are supported.")
@@ -30,7 +30,7 @@ class SeizureData():
 
         self.channels = self.eeg.channels
 
-        self.window_size = int(window_size_sec*fs)
+        self.window_size = int(window_size_sec * fs)
         self.fs = fs
         self.recording_duration = int(self.data.shape[1] / fs)
 
@@ -44,8 +44,12 @@ class SeizureData():
         self.notch_60_b = notch_60_b
         self.notch_60_a = notch_60_a
 
-        window_idx = np.arange(0, self.recording_duration*self.fs, self.fs).astype(int)
-        self.window_idx = window_idx[window_idx < self.recording_duration*self.fs - self.window_size]
+        window_idx = np.arange(0, self.recording_duration * self.fs, self.fs).astype(
+            int
+        )
+        self.window_idx = window_idx[
+            window_idx < self.recording_duration * self.fs - self.window_size
+        ]
 
     def __len__(self):
         return len(self.window_idx)
@@ -59,28 +63,47 @@ class SeizureData():
         nyq = 0.5 * self.fs
         low = self.lowcut / nyq
         high = self.highcut / nyq
-        b, a = butter(order, [low, high], btype='band')
+        b, a = butter(order, [low, high], btype="band")
         y = lfilter(b, a, data)
         return y
 
     def preprocess_clip(self, eeg_clip):
         bandpass_filtered_signal = self.butter_bandpass_filter(eeg_clip, order=3)
-        filtered_1_signal = lfilter(self.notch_1_b, self.notch_1_a, bandpass_filtered_signal)
-        filtered_60_signal = lfilter(self.notch_60_b, self.notch_60_a, filtered_1_signal)  
+        filtered_1_signal = lfilter(
+            self.notch_1_b, self.notch_1_a, bandpass_filtered_signal
+        )
+        filtered_60_signal = lfilter(
+            self.notch_60_b, self.notch_60_a, filtered_1_signal
+        )
         eeg_clip = filtered_60_signal
         return eeg_clip
 
     def get_a_clip(self, idx):
-        eeg_clip = self.data[:, self.window_idx[idx]:self.window_idx[idx]+self.window_size]
-        mask_clip = self.mask[self.window_idx[idx]:self.window_idx[idx]+self.window_size]
+        eeg_clip = self.data[
+            :, self.window_idx[idx] : self.window_idx[idx] + self.window_size
+        ]
+        mask_clip = self.mask[
+            self.window_idx[idx] : self.window_idx[idx] + self.window_size
+        ]
         return eeg_clip, mask_clip
 
 
 if __name__ == "__main__":
     data_root = "/data/jjiang10/Data/EEG/BIDS_Siena"
-    
-    train_cases = ['sub-00', 'sub-01', 'sub-03', 'sub-05', 'sub-06', 'sub-07', 'sub-09','sub-10', 'sub-11', 'sub-12']
-    test_cases = ['sub-13', 'sub-14', 'sub-16', 'sub-17']
+
+    train_cases = [
+        "sub-00",
+        "sub-01",
+        "sub-03",
+        "sub-05",
+        "sub-06",
+        "sub-07",
+        "sub-09",
+        "sub-10",
+        "sub-11",
+        "sub-12",
+    ]
+    test_cases = ["sub-13", "sub-14", "sub-16", "sub-17"]
 
     for case in test_cases:
         data_dir = os.path.join(data_root, case)
@@ -94,9 +117,6 @@ if __name__ == "__main__":
                     label_file = edf_file.replace("eeg.edf", "events.tsv")
                     eeg_dataset = SeizureData(edf_file, label_file)
                     print(len(eeg_dataset))
-                    print(eeg_dataset[0][0].shape) # torch.Size([19, 6400])
+                    print(eeg_dataset[0][0].shape)  # torch.Size([19, 6400])
 
-                    
     print("Done")
-
-
